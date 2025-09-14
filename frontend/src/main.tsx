8bc2638083a547bc85f698a7540ad84f9d3d35e1
vanshkatiyar441 <vansh.katiyar.786@gmail.com>
2025-09-14 18:09:52 +0530

    1

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+console.log('VITE_API_BASE_URL:', import.meta.env.VITE_API_BASE_URL);
+
 createRoot(document.getElementById("root")!).render(<App />);
